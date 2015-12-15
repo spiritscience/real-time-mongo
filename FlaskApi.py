d433f8c669e29bf1728d85aeccb7811334d8d05a
Taiiwo Llort <tylerzacc@gmail.com>
2015-12-15 12:38:34 -0500

    add send message function to API

--- FlaskApi.py
+++ FlaskApi.py
@@ -9,9 +9,12 @@ rtm = RTM()
 @app.route('/send')
 def send_message():
     if request.method == "POST":
-        data = request.form['data']
-        crate = json.loads(data)
-        rtm.send(crate, 'messages' visible=True)
+        data = str(request.form['data'])
+        sender_pair = request.form['sender_pair']
+        recipient = request.form['recipient']
+        collection = "messages"
+        
+        rtm.send(data, sender_pair, recipient, collection)
 
 @app.route('/')
 def index():
