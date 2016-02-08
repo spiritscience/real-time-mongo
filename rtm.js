94c615fbac269f09b4685d10df896b695c2736ef
Not Eric Moon <notEric@mailinator.com>
2016-02-07 16:29:09 -0800

    add the send function

--- rtm.js
+++ rtm.js
@@ -1,28 +1,47 @@
 function RTM(url){
     this.url = url;
+    // Connect to socketserver
+    this.ws = new WebSocket(this.url);
+    this.handlers = [];
 
-    // Open a socket listening for database changes
+    // define the message callback
+    this.message_callback = function(message){
+        // iterate the handlers
+        for (var i in this.handlers){
+            handler = this.handlers[i];
+            // run the handlers
+            handler(message);
+        }
+        $(this).trigger('eachDocument');
+    }
+    // set the message callback
+    this.ws.onmessage = this.message_callback;
+
+    // Tells the server to listen for database changes and send them to us
     this.listen = function(collection, sender_pair, recipient_pairs, callback){
         var callback = callback || false;   // make callback optional
-        // Connect to socketserver
-        var ws = new WebSocket(this.url);
         // set the response callback
-        ws.onmessage = function(data){
+        var handler = function(data){
             // run the callback
             if (callback) {
                 callback(data);
             }
             // trigger the event
-            $(this).trigger('eachDocument');
         };
+        this.handlers.push(handler)
         query = {
             collection: collection,
             sender_pair: sender_pair,
             recipient_pairs: recipient_pairs
         };
-        ws.onopen = function(){
-            ws.send('LISTEN: ' + JSON.stringify(query));
-        }
+        ws.send('LISTEN: ' + JSON.stringify(query));
     }
     
+    this.send = function(collection, sender_pair, recipient_pairs){
+        this.ws.send("SEND: " + JSON.stringify({
+            collection: collection,
+            sender_pair: sender_pair,
+            recipient_pairs: recipient_pairs
+        });
+    }
 }
