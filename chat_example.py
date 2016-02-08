d4a661c1b5d0cb83588a994bd79b1f885d87ded8
Not Eric Moon <notEric@mailinator.com>
2016-02-07 16:02:54 -0800

    combine send function with socket server

--- chat_example.py
+++ chat_example.py
@@ -1,20 +1,20 @@
 import RTM
 import time
+import threading
 
 # runs for each message sent by each client
-def handler(websocket):
-    recv = yield from websocket.recv()
-    if recv[:8] == 'LISTEN: ':
+def listen_handler(websocket, recv):
+    if recv[:len("listen: ")].lower() == 'listen: ':
         data = recv[8:]
         try:
             req = json.loads(data)
         except:
-            yield from websocket.send("Invalid request: Not JSON")
+            yield from websocket.send("listen: Not JSON")
             return False
         # check we have all the values we need
         if not "recipient_pairs" in req or not 'collection' in req \
                 or not 'sender_pair' in req:
-            yield from websocket.send("Invalid request: Wrong params")
+            yield from websocket.send("listen: Wrong params")
             return False
         # recipients info
         recipient_pairs = req['recipient_pairs']
@@ -23,14 +23,13 @@ def handler(websocket):
         # collection is the name of the collection
         collection = req['collection']
         # find the documents addressed to any of the recipients
-        mongo = RTM.MongoConnection()
         stream = mongo.message_stream(
             sender_pair,
             recipient_pairs,
             collection
         )
         if not stream:
-            yield from websocket.send("Invalid request: Invalid recipient")
+            yield from websocket.send("listen: Invalid recipient")
             return False
         # this for loop bocks, so nothing after it will be executed.
         for message in stream:
@@ -45,10 +44,31 @@ def handler(websocket):
                 }
                 yield from websocket.send(json.dumps(data))
 
+def send_handler(websocket, recv):
+    # if the first part of the line is "Send :"
+    if recv[:len("send: ")].lower() == "send: ":
+        data = json.loads(recv[len("send: "):])
+        if not "sender_pair" in data or not "recipient" in data or not \
+                "collection" in data:
+            yield from websocket.send("send: Invalid Arguments")
+        sender_pair = data['sender_pair']
+        recipient = data['recipient']
+        collection = "messages"
+        
+        mongo.send(data, sender_pair, recipient, collection)
+        yield from websocket("send: Message sent")
+
+def handler(websocket):
+    recv = yield from websocket.recv()
+    threading.Thread(target=listen_handler, args=(websocket, recv)).start()
+    threading.Thread(target=send_handler, args=(websocket, recv)).start()
+
+# connection to the mongo db
+mongo = RTM.MongoConnection()
 # instantiate a websocket server
 wss = RTM.WSServer(debug=True)
 # add our threads to the websocket server
-wss.add_send_thread(handler)
+wss.add_handler(handler)
 
 # start the websocket server. This method never returns
 wss.start()
